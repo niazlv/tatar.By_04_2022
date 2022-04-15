30e60cd24b8a3a83c09eb4c5cc79d28b0c1aa275
Niyaz <niazlv03@gmail.com>
2022-04-15 21:30:41 +0300

    init

---
+++ app/src/main/java/com/example/sberteacher/LevelActivity.java
@@ -0,0 +1,25 @@
+package com.example.sberteacher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class LevelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_level);
+
+        ImageView mL1 = findViewById(R.id.l1);
+        mL1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LevelActivity.this, GameActivity.class));
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/sberteacher/MainActivity.java
+++ app/src/main/java/com/example/sberteacher/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.sberteacher;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,43 +20,41 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.Timer;
+
 public class MainActivity extends AppCompatActivity {
+
+    ImageView mBackground, mStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final FirebaseAuth mAuth;
-        mAuth = FirebaseAuth.getInstance();
+        mBackground = findViewById(R.id.background);
+        mStart = findViewById(R.id.start);
+        ObjectAnimator animation = ObjectAnimator.ofFloat(mBackground, "translationX", -600f);
+        animation.setDuration(2000);
+        animation.start();
 
-        final EditText mEmail = findViewById(R.id.email);
-        final EditText mPassword = findViewById(R.id.password);
+        new CountDownTimer(2000, 1000){
 
-        Button mButtonEnter = findViewById(R.id.button_enter);
-        mButtonEnter.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                String email = mEmail.getText().toString().trim();
-                String password = mPassword.getText().toString().trim();
+            public void onTick(long millisUntilFinished) {
+            }
 
-                if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)){
-                    Toast.makeText(MainActivity.this, "Не все поля заполнены", Toast.LENGTH_SHORT).show();
-                } else{
-                    mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()){
-                                Intent intent = new Intent(MainActivity.this, TeacherActivity.class);
-                                startActivity(intent);
-                            } else{
-                                Toast.makeText(MainActivity.this, "Что-то не так. Попробуйте снова", Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    });
-                }
-;           }
+            @Override
+            public void onFinish() {
+                mStart.setVisibility(View.VISIBLE);
+            }
+        }.start();
+
+        mStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LevelActivity.class));
+            }
         });
-
     }
+
 }
