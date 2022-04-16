729587f1289e0578fda625aee848fc9f119786b8
Anastasia Lavrova <WISHNYA23@GMAIL.COM>
2022-04-16 03:10:32 +0300

    added alert dialog

--- app/src/main/java/com/example/sberteacher/GameActivity.java
+++ app/src/main/java/com/example/sberteacher/GameActivity.java
@@ -1,14 +1,19 @@
 package com.example.sberteacher;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class GameActivity extends AppCompatActivity {
     ImageView im;
@@ -57,6 +62,26 @@ public class GameActivity extends AppCompatActivity {
                 @Override
                 public boolean onLongClick(View view) {
                     st = "Hold on "+String.valueOf(finalI)+" btn";
+
+                    //create alert dialog
+                    AlertDialog.Builder builder = new AlertDialog.Builder(GameActivity.this);
+                    view = (ConstraintLayout) getLayoutInflater()
+                            .inflate(R.layout.alert_item_info, null);
+
+                    ImageView mDictionary = view.findViewById(R.id.dictionary);
+                    ImageView mCloseAlert = view.findViewById(R.id.close_alert);
+                    TextView mRus = view.findViewById(R.id.text_rus);
+                    TextView mTat = view.findViewById(R.id.text_tat);
+                    ImageView mImage = view.findViewById(R.id.mImage);
+
+
+                    builder.setView(view);
+                    AlertDialog dialog
+                            = builder.create();
+                    dialog.getWindow().setBackgroundDrawableResource(R.color.translucent_black);
+                    dialog.show();
+
+
                     Toast.makeText(GameActivity.this,st,Toast.LENGTH_SHORT).show();
                     return true;
                 }
